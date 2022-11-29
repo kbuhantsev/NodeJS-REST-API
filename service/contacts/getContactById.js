61ba63d865feadc13403597c6d31a7e9f105bd32
Konstantyn <k.buhantsev@gmail.com>
2022-11-29 16:14:58 +0200

    Update getContactById.js

--- service/contacts/getContactById.js
+++ service/contacts/getContactById.js
@@ -1,7 +1,12 @@
 const Contact = require("../models/contact");
+const { NotFound } = require("http-errors");
 
 const getContactById = async (contactId) => {
-  return await Contact.findById(contactId);
+  const result = await Contact.findById(contactId);
+  if (!result) {
+    throw NotFound(`Can not find contact with ID:${contactId}`);
+  }
+  return result;
   // return await Contact.findOne({ _id: contactId });
 };
 
