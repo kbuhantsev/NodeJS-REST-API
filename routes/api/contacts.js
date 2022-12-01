b77278de49a02bb71c6cb5b63736caf99339b53b
Konstantyn <k.buhantsev@gmail.com>
2022-12-01 13:22:27 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,27 +3,34 @@ const router = express.Router();
 
 const { contacts: ctrl } = require("../../controllers");
 
-const { ctrlWrapper, validator, isValidId } = require("../../middlewares");
+const {
+  ctrlWrapper,
+  validator,
+  isValidId,
+  auth,
+} = require("../../middlewares");
 const {
   addContactSchema,
   patchFavoriteSchema,
 } = require("../../models/contact");
 
 router
-  .get("/", ctrlWrapper(ctrl.allContacts)) //
-  .post("/", validator(addContactSchema), ctrlWrapper(ctrl.newContact));
+  .get("/", auth, ctrlWrapper(ctrl.allContacts)) //
+  .post("/", auth, validator(addContactSchema), ctrlWrapper(ctrl.newContact));
 
 router
-  .get("/:contactId", isValidId, ctrlWrapper(ctrl.contactById))
-  .delete("/:contactId", isValidId, ctrlWrapper(ctrl.deleteContact))
+  .get("/:contactId", auth, isValidId, ctrlWrapper(ctrl.contactById))
+  .delete("/:contactId", auth, isValidId, ctrlWrapper(ctrl.deleteContact))
   .put(
     "/:contactId",
+    auth,
     isValidId,
     validator(addContactSchema),
     ctrlWrapper(ctrl.putContact)
   )
   .patch(
     "/:contactId/favorite",
+    auth,
     isValidId,
     validator(patchFavoriteSchema),
     ctrlWrapper(ctrl.patchFavorite)
